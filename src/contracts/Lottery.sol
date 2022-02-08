b7a97aa1280fc21fc76c4febb363cca9141138b6
Edward Barak <edward.barak@gmail.com>
2022-02-07 20:52:12 -0500

    sketch out functionality of functions

--- src/contracts/Lottery.sol
+++ src/contracts/Lottery.sol
@@ -29,13 +29,60 @@ contract Lottery {
     }
 
     //FUNCTIONS
-    function buyTicket() public payable noReentrant {}
+    function buyTicket() public payable noReentrant {
+        /*
+        if lottery is active:
+            transfer eth from msg.sender to contract
+            create ticket for msg.sender for the current lottery
+        else:
+            error("lottery is not active")
+        */
+    }
 
-    function withdrawWinnings() public payable noReentrant {}
+    function withdrawWinnings() public payable noReentrant {
+        /*
+        if msg.sender has winnings:
+            transfer all eth from contract to msg.sender
+        else:
+            error("no claimable winnings")
+        */
+    }
 
-    function withdrawFees() public payable noReentrant isOwner {}
+    function withdrawFees() public payable noReentrant isOwner {
+        /*
+        if fees > 0:
+            transfer all fees from contract to msg.sender
+        else:
+            error("no claimable fees")
+        */
+    }
 
-    function startLottery() public isOwner {}
+    function startNewLottery() public isOwner noReentrant {
+        /*
+        if block.timestamp >= lotteryEnd:
+            selectCurrentLotteryWinner()
+            start new lottery 
+                currentLottery += 1
+                lotteryEnd = block.timestamp + lotteryLifetime
+        else:
+            error("current lottery has not ended yet")
+        */
+    }
 
-    function endLottery() public isOwner {}
+    function selectCurrentLotteryWinner() internal {
+        /*
+        _randNum = select random number between 0 and tickets[currentLottery].length
+        winner = tickets[currentLottery][_randNum]
+        winners[winner] += currentLotteryPot
+        currentLotteryPot = 0
+        */
+    }
+
+    function retireLotteryContract() public isOwner {
+        /*
+        selectCurrentLotteryWinner()
+        withdrawFees()
+        only allow winners to withdraw winnings
+        */
+    }
 }
