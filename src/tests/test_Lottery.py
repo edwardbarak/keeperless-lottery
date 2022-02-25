09791ad5f35916574886fda06d28249a09dcf56f
Edward Barak <edward.barak@gmail.com>
2022-02-25 16:18:27 -0500

    write test for buyTicket()

--- src/tests/test_Lottery.py
+++ src/tests/test_Lottery.py
@@ -5,3 +5,19 @@ from scripts.deploy import deploy_lottery
 def test_correct_owner():
     lottery = deploy_lottery()
     assert lottery.owner() == accounts[0]
+
+
+def test_buyTicket():
+    # ARRANGE
+    lottery = deploy_lottery()
+    initialContractBalance = lottery.balance()
+
+    # ACT
+    lottery.buyTicket({"from": accounts[1], "value": lottery.ticketPrice()})
+
+    # ASSERT
+    # assert that purchaser owns a ticket
+    assert lottery.lotteryTickets(lottery.currentLottery(), 0) == accounts[1]
+
+    # assert that contract received eth for the ticket
+    assert lottery.balance() == initialContractBalance + lottery.ticketPrice()
